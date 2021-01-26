3cbf8eae8d34f61ee5c714987221a0efb748e1b3
marmat001 <markusmatu@Markuss-MacBook-Pro.local>
2021-01-26 12:45:21 +0100

    created shop page and added the collection preview of the items, and the individual item component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/Homepage';
+import ShopPage from './pages/ShopPage';
 import './sass/app.scss';
-
 
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
-				<Route path="/hats" component={HatsPage} />
+				<Route path="/shop" component={ShopPage} />
 			</Switch>
 		</div>
 	);
