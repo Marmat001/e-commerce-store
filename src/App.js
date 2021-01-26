e826a3d559fe945dd006a5a6ffc583cc40da6ff4
marmat001 <markusmatu@Markuss-MacBook-Pro.local>
2021-01-26 12:54:47 +0100

    created navigation component and the links for the different routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import HomePage from './pages/Homepage';
 import ShopPage from './pages/ShopPage';
 import './sass/app.scss';
@@ -7,6 +8,7 @@ import './sass/app.scss';
 function App() {
 	return (
 		<div>
+			<Navigation />
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
